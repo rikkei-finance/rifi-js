3f5b476f614cd715ed196f7a8667efb34bf92e12
Pham Ngoc Chuyen <chuyenbka@gmail.com>
2021-09-21 19:17:13 +0700

    add nft vault contract

--- src/constants.ts
+++ src/constants.ts
@@ -98,6 +98,7 @@ export const address = {
     "RewardLocker": "0x9653811B43D30b894310bD9656D24363473a7825",
     "VaultBusdVenus": "0xcc98b38F2f5d80d561Da8AA060f70Eb003FcAB87",
     "VaultUsdtVenus": "0x4617E38A8A1c5dfBF8a3916e4227732BFe143b0f",
+    "KabyNFTVault": "0xA07505754bDE2879BD47573F465CF727C9D0C735",
   },
 
   "rinkeby": {
